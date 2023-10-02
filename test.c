561f6031888ff3abcd3b94dd13bb8b7f26f74ac2
Albert Mendez <am20170484@gmail.com>
2023-10-01 20:35:45 -0500

    first commit

---
+++ test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <assert.h>
+
+#define TRACESTACK
+
+#define   F0 0
+#define SIZE = 7;
+
+
+int main()
+{
+	
+	
+	printf("%5.1f", 3223339.1);
+	return 0;
+}
+
